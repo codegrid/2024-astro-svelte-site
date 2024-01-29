e7f6f274fe082ee55e84021d59fd71baf0b832a7
Yu Watanabe <watanabe.yu.1203@gmail.com>
2024-01-29 14:13:52 +0900

    サイトイメージをmainブランチに作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  site: 'https://codegrid.github.io',
+  base: '/2024-astro-svelte-site',
 });

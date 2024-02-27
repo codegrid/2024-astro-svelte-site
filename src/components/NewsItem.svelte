8dc1c1114687e686c76744969087f42074a9b0a8
Yu Watanabe <watanabe.yu.1203@gmail.com>
2024-02-27 14:29:10 +0900

    お知らせページを追加

---
+++ src/components/NewsItem.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let slug = "";
+  export let title = "";
+
+  let isRead = false;
+
+  onMount(() => {
+    const readItemData = window.localStorage.getItem("readItemList") || "[]";
+    const readItemList = JSON.parse(readItemData);
+
+    isRead = readItemList.includes(`/news/${slug}`);
+  });
+</script>
+
+<li class="newsList_item">
+  <a href={`/news/${slug}`} class={isRead ? "--read" : ""}>
+    {title}
+    {#if isRead}
+      <span class="newsList_item_read"> ✅ </span>
+    {/if}
+  </a>
+</li>
+
+<style>
+  .newsList_item {
+    margin-block: 15px;
+  }
+
+  .--read {
+    color: #777;
+  }
+
+  .newsList_item_read {
+    display: inline-block;
+    margin-left: 5px;
+  }
+</style>

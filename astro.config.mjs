8bbc65d85f8c135ec9280efd89d6778432c0c263
Yu Watanabe <watanabe.yu.1203@gmail.com>
2024-04-23 15:08:56 +0900

    公開サイトのパスを反映

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
+  site: 'https://codegrid.github.io',
+  base: '/2024-astro-svelte-site/',
   vite: {
     build: {
       rollupOptions: {
